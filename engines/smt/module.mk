a6b3e749372441f0bea5fb26990e20c8af6b24de
monyarm <monyarm@gmail.com>
2021-02-14 22:48:53 +0200

    PAC Support

--- engines/smt/module.mk
+++ engines/smt/module.mk
@@ -3,9 +3,15 @@ MODULE := engines/smt
 MODULE_OBJS := \
 	smt.o \
 	gfx.o \
+	gfx_opengl.o \
+	gfx_opengl_texture.o \
+	gfx_opengl_shaders.o \
+	gfx_tinygl.o \
+	gfx_tinygl_texture.o \
 	detection.o \
 	formats/archive/cpk.o \
 	formats/archive/cvm.o \
+	formats/archive/pac.o \
 	formats/video/pmsf.o \
 	formats/image/tmx.o \
 	formats/image/dds.o \
